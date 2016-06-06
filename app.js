33ca02a3e34fa7ebd879a69239cc51cfce2f113f
Andrey Maznyak <andreymaznyak@gmail.com>
2016-06-06 18:36:32 +1000

    first injection angular

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
  * Created by AndreyMaznyak on 06.06.2016.
  */
 'use strict';
-document.onload = function(){
-    alert('Загружена страница');
-}
+var app = angular.module('andreymaznyak.github.io',[]);
+
+app.config(function onConfig(){
+
+});
+
+app.run(function onRun(){
+    console.log('angular started');
+});
